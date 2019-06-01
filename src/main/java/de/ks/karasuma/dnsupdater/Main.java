5ab65076cbcef8d1f2f8c172102082ab6969f462
Karasuma1412 <bjoerninorek@gmail.com>
2019-06-01 16:55:38 +0200

    updater for ddnss

---
+++ src/main/java/de/ks/karasuma/dnsupdater/Main.java
@@ -0,0 +1,31 @@
+package de.ks.karasuma.dnsupdater;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.getProperties().setProperty("java.net.preferIPv6Addresses",
+                "true");
+        updateDDNS(args[0]);
+    }
+
+    private static void updateDDNS(String arg) {
+        try {
+            URL url = new URL(arg);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(5000);
+            urlConnection.setRequestProperty("User-Agent","Chrome/70.0.3538.77");
+            urlConnection.connect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
